960cf2e2299c75f32384da2ff5abff5bb49a6f72
Rakesh4538 <113422381+Rakesh4538@users.noreply.github.com>
2024-06-03 19:56:46 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (55.55%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.java
+++ 328-odd-even-linked-list/odd-even-linked-list.java
@@ -10,19 +10,25 @@
  */
 class Solution {
     public ListNode oddEvenList(ListNode head) {
-
-        if(head == null) return head;
-        ListNode odd = head;
-        ListNode even = odd.next;
-        ListNode evenHead = even;
-
-        while(even != null && even.next != null) {
-            odd.next = even.next;
-            odd = odd.next;
-            even.next = odd.next;
-            even = even.next;
+        if(head == null || head.next == null || head.next.next == null)
+        {
+            return head;
         }
-        odd.next = evenHead;
+        ListNode odd = head;
+        ListNode even = head.next;
+
+        ListNode evenStart = even; //This will be a marker of even-node start-point,using it we can join set of evennodes after the odd nodes
+        
+        while(odd.next != null && even.next != null){
+            odd.next = even.next;
+            odd = even.next;
+            if(odd != null){
+                even.next = odd.next;
+                even = odd.next;
+                
+            }
+        }
+        odd.next = evenStart;
         return head;
 
         
